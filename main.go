59f9a8d8b2c3255dfa7dcd33008fbf14320b671b
Matt Hook <hookenz@gmail.com>
2021-10-21 09:20:03 +1300

    Meanwhile main moves on

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func helloWorld() {
-	fmt.Println("Hello world")
+func helloWorld(n string) {
+	fmt.Println("Hello %d and Hello world", n)
 }
 
 func main() {
-	helloWorld()
+	helloWorld("Jane")
 	return 0
 }
