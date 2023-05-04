6db360b093280ab6c681afa2cfaf8703bcd0d68d
Matt Hook <hookenz@gmail.com>
2023-05-05 10:39:33 +1200

    embedded test

--- api/main.go
+++ api/main.go
@@ -6,7 +6,20 @@ import (
 	"github.com/hookenz/mytest/pkg/libhello"
 )
 
+type (
+	Embedded struct {
+		Name  string
+		Value string
+	}
+
+	MyStruct struct {
+		Embedded
+	}
+)
+
 func main() {
-	fmt.Println("Hello")
+	m := MyStruct{}
+	m.Name = "Bob"
+	fmt.Println("Hello", m.Name)
 	libhello.Hello()
 }
