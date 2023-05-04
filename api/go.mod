bff1ed61b661d4ca7de676729cb4b410b5e65db8
Matt Hook <hookenz@gmail.com>
2023-05-05 10:47:43 +1200

    try to break again

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,7 @@ module github.com/hookenz/mytest/api
 
 go 1.19
 
-require github.com/hookenz/mytest/pkg/libhello v1.2.6
+require (
+	github.com/fvbommel/sortorder v1.1.0
+	github.com/hookenz/mytest/pkg/libhello v1.2.6
+)
--- api/main.go
+++ api/main.go
@@ -2,24 +2,68 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/fvbommel/sortorder"
 	"github.com/hookenz/mytest/pkg/libhello"
 )
 
 type (
-	Embedded struct {
-		Name  string
-		Value string
+	Pair struct {
+		Name  string `json:"name" example:"name"`
+		Value string `json:"value" example:"value"`
 	}
-
-	MyStruct struct {
-		Embedded
+	InstanceType struct {
+		Pair
 	}
 )
 
+type InstanceTypeByName []InstanceType
+
+func (t InstanceTypeByName) Len() int {
+	return len(t)
+}
+
+func (t InstanceTypeByName) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t InstanceTypeByName) Less(i, j int) bool {
+
+	// Put General Purpose instance types first
+	if strings.HasPrefix(t[i].Name, "General Purpose") {
+		if !strings.HasPrefix(t[j].Name, "General Purpose") {
+			return true
+		}
+	} else if strings.HasPrefix(t[j].Name, "General Purpose") {
+		return false
+	}
+
+	return sortorder.NaturalLess(t[i].Name, t[j].Name)
+}
+
 func main() {
-	m := MyStruct{}
-	m.Name = "Bob"
-	fmt.Println("Hello", m.Name)
+	types := []InstanceType{
+		InstanceType{
+			Pair: Pair{
+				Name:  "General Purpose",
+				Value: "t3.medium",
+			},
+		},
+		InstanceType{
+			Pair: Pair{
+				Name:  "Compute Optimized",
+				Value: "c5.large",
+			},
+		},
+		InstanceType{
+			Pair: Pair{
+				Name:  "Memory Optimized",
+				Value: "r5.large",
+			},
+		},
+	}
+
+	fmt.Printf("Types: %v\n", types)
 	libhello.Hello()
 }
